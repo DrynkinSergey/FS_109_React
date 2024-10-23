89a200185150c83d8c0ece2fac3ed86892accae1
Sergey <sergsmile29@gmail.com>
2024-10-23 21:00:54 +0300

    clsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
+import clsx from 'clsx';
 import s from './Header.module.css';
 const Header = () => {
   return (
     <header className='header'>
       <div>Logo</div>
       <p className={s.title}>Hello</p>
-      <p className={s.title}>World</p>
+      {/* <p className={`${s.title} ${s.second_class}`}>World</p> */}
+      <p className={clsx(s.title, s.second_class, 'active', 'hello', 'world')}>World</p>
       <nav className={s.wrapper}>
         <ul className='nav'>
           <li>Home</li>
