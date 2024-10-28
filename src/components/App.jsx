254180d112c023a4588c50d95e8eec2479fa620e
Sergey <sergsmile29@gmail.com>
2024-10-28 02:56:14 +0200

    init m2l1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,15 @@
-import Button from './Button/Button';
-import Modal from './Modal/Modal';
-import Header from './Header/Header';
-import 'modern-normalize';
-import Message from './Message/Message';
-import animals from './../assets/animals.webp';
-import '../index.css';
-import List from './List/List';
-import Profile from './Profile/Profile';
-import profileData from './../assets/user.json';
+import { ColorPicker } from './ColorPicker/ColorPicker';
+import { Counter } from './Counter/Counter';
+import { TodoList } from './TodoList/TodoList';
+
 const App = () => {
-  const isOnline = false;
-  const isLoading = false;
-  const age = 44;
-  // console.log(`Hello world, my age is ${age}`);
-  console.log(`Hello world, my age is ${age}`);
-
-  const filmsData = [
-    {
-      id: '1',
-      title: 'Terminator',
-    },
-
-    {
-      id: '2',
-      title: 'Spiderman',
-    },
-    {
-      id: '3',
-      title: 'Batman',
-    },
-  ];
-  const goodsData = [
-    {
-      id: '1',
-      title: 'Морква',
-    },
-
-    {
-      id: '2',
-      title: 'Капуста',
-    },
-    {
-      id: '5',
-      title: 'Банку огірків',
-    },
-    {
-      id: '3',
-      title: 'Iphone',
-    },
-  ];
   return (
     <div>
-      <Header />
-      {isOnline && <h1>Welcome back!</h1>}
-      {isLoading && <h2>Loading...</h2>}
-      {age > 18 ? <h2 className='title'>Ти диви який вимахав!</h2> : <h2>Йди їж кашу</h2>}
-      <List title='My films' data={filmsData} />
-      <List title='Goods' data={goodsData} />
-      <section>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatem veritatis! Minus fugit sit itaque, cumque exercitationem sequi.
-        Dolorum maxime rem possimus sapiente harum exercitationem delectus voluptate consectetur aut nam.
-      </section>
-
-      <Modal title='Children how it works'>
-        <h2>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non id eaque assumenda cum ex asperiores ipsum ullam, in magni, numquam, deserunt
-          eum quas inventore exercitationem animi consectetur. Eos, amet quod.
-        </h2>
-        <button>CLICK</button>
-      </Modal>
-
-      <Modal title='Login'>
-        <input />
-        <input />
-        <button>CLICK</button>
-      </Modal>
-      <img src={animals} />
-      <Modal title='Messages box'>
-        <Message classn text='Куплю праску!' author='Олена' />
-        <Message text='Продам телевізор' author='Макс' />
-        <Message text='Куплю шапку' author='Ігор' />
-      </Modal>
-
-      <Button>Click</Button>
-      <Button>🔥 Click</Button>
-      <Profile user={profileData} />
+      <h1>State</h1>
+      {/* <Counter />
+      <ColorPicker />
+      <TodoList /> */}
     </div>
   );
 };
-
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,15 @@
-const Modal = props => {
+import s from './Modal.module.css';
+const Modal = ({ children, title = 'Default modal' }) => {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      {props.children}
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <>
+          <h1>{title}</h1>
+          <hr />
+        </>
+        <button className={s.closeBtn}>×</button>
+        {children}
+      </div>
     </div>
   );
 };
