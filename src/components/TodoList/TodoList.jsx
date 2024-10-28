fecc1df8340d377f94fa512be6db6456e9f4dbb0
Sergey <sergsmile29@gmail.com>
2024-10-28 21:26:52 +0200

    todolist logic done

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import todosData from './../../assets/todos.json';
 import { TodoItem } from './TodoItem';
 import s from './TodoList.module.css';
+import { nanoid } from 'nanoid';
 export const TodoList = () => {
+  const [todos, setTodos] = useState(todosData);
+  const [newValue, setNewValue] = useState('');
+  const handleDeleteTodo = id => {
+    setTodos(prev => prev.filter(item => item.id !== id));
+  };
+  const handleAddTodo = () => {
+    const newTodo = {
+      id: crypto.randomUUID(),
+      todo: newValue,
+      completed: false,
+    };
+    setTodos(prev => [...prev, newTodo]);
+  };
+
   return (
     <>
       <div className='flex'>
-        <input className={s.input} />
-        <button className='btn border'>Add</button>
+        <input value={newValue} onChange={e => setNewValue(e.target.value)} className={s.input} />
+        <button onClick={handleAddTodo} className='btn border'>
+          Add
+        </button>
       </div>
       <ul className={s.list}>
-        {todosData.map(item => (
-          <TodoItem key={item.id} {...item} />
+        {todos.map(item => (
+          <TodoItem key={item.id} {...item} handleDeleteTodo={handleDeleteTodo} />
         ))}
       </ul>
     </>
