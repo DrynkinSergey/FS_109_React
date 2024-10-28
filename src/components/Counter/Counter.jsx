aa1f6d4370dc1e6ee04da0244c4c6c101bcc0a32
Sergey <sergsmile29@gmail.com>
2024-10-28 20:36:05 +0200

    useState

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,26 +1,41 @@
+import { useState } from 'react';
 import s from './Counter.module.css';
 
 export const Counter = () => {
-  const sayMyName = e => {
-    console.log('Hello');
-    console.log(e);
+  const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(111);
+  const handlePlusClick = () => {
+    // setCounter(0 + 1);
+    // setCounter(0 + 1);
+    // setCounter(0 + 1);
+    setCounter(prev => prev + step);
+    console.log(counter);
   };
-  const sayMyNameWithName = name => {
-    console.log(`Hello, ${name}`);
+  const handleMinusClick = () => {
+    setCounter(prev => prev - step);
   };
 
+  const handleReset = () => {
+    setCounter(0);
+    setStep(1);
+  };
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
-        <h1>{1}</h1>
+        <h1>{counter}</h1>
+        <input
+          onChange={e => {
+            setStep(+e.target.value);
+          }}
+        />
         <div className={s.flex}>
-          <button className='btn' onClick={() => sayMyNameWithName('Serhii')}>
+          <button className='btn' onClick={handleMinusClick}>
             minus
           </button>
-          <button className='btn' onClick={sayMyName}>
+          <button className='btn' onClick={handleReset}>
             reset
           </button>
-          <button className='btn' onClick={() => console.log('Hello world')}>
+          <button className='btn' onClick={handlePlusClick}>
             plus
           </button>
         </div>
