1c1e581d03fdc32c375fd2d1bc8c1b89c5c8b6ed
Sergey <sergsmile29@gmail.com>
2024-11-06 19:54:43 +0200

    base example of formik

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,4 +1,35 @@
+import { Field, Form, Formik } from 'formik';
+
 const OrderForm = () => {
-  return <div>Order form</div>;
+  const handleSubmit = (values, options) => {
+    console.log(values);
+  };
+  const initialValues = {
+    username: '',
+    tel: '',
+    email: '',
+  };
+
+  return (
+    <div>
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        <Form>
+          <label>
+            <span>Ім'я</span>
+            <Field type='text' placeholder="Введіть ім'я:" name='username' />
+          </label>
+          <label>
+            <span>Телефон</span>
+            <Field type='text' placeholder='Введіть Телефон:' name='tel' />
+          </label>
+          <label>
+            <span>Емейл</span>
+            <Field type='text' placeholder='Введіть Емейл:' name='email' />
+          </label>
+          <button type='submit'>Замовити</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 export default OrderForm;
