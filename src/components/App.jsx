e71bebb0e4015bad7b674e17ff938ec861a82407
Sergey <sergsmile29@gmail.com>
2024-11-11 20:27:01 +0200

    loading, error, view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,39 @@
+import Articles from './Articles/Articles';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { fetchArticles } from '../services/api';
+import Loader from './Loader/Loader';
+
 const App = () => {
-  return <div>HTTP requests</div>;
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const { hits } = await fetchArticles();
+        setArticles(hits);
+      } catch (error) {
+        console.error(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      <Articles articles={articles} />
+      {isError && <h2>Щось сталось! Онови сторінку...</h2>}
+    </div>
+  );
 };
 export default App;
+//   axios.get('https://hn.algolia.com/api/v1/search?query=react').then(res => setArticles(res.data.hits));
