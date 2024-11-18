f23361dbe82f5259ade4b9ae3ed66b012edc2709
Sergey <sergsmile29@gmail.com>
2024-11-18 19:53:16 +0200

    Step 2. Add Routes and some Route with path and element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<h2>Homepage</h2>} />
+        <Route path='/about' element={<h2>About page</h2>} />
+      </Routes>
     </div>
   );
 };
