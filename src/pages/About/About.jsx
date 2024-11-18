4e5e007c970691f1cb534cdef7d4a63f1d5a1d3c
Sergey <sergsmile29@gmail.com>
2024-11-18 20:18:35 +0200

    Offtop: add title to document

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from 'react';
+
 const About = () => {
+  useEffect(() => {
+    document.title = 'BEST CODERS | About';
+  }, []);
   return <div>About page</div>;
 };
 export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from 'react';
+
 const Home = () => {
+  useEffect(() => {
+    document.title = 'BEST CODERS | HOME';
+  }, []);
   return <div>HomePage</div>;
 };
 export default Home;
