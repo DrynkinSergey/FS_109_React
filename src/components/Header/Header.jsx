a873783f2529465b42ffb2291609c3a9823d7b07
Sergey <sergsmile29@gmail.com>
2024-11-18 20:04:35 +0200

    Step 4. Add links for navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
 import s from './Header.module.css';
 const Header = () => {
   return (
     <header className={s.header}>
       <h3>Routing</h3>
+      <nav className={s.nav}>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
     </header>
   );
 };
