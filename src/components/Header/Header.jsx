db09684b9f11e33d7a62ccfa5345cfcd32110471
Sergey <sergsmile29@gmail.com>
2024-12-11 19:13:57 +0200

    update styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import s from './Header.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLoggedIn, selectUser } from '../../redux/auth/selectors';
 import { logout } from '../../redux/auth/operations';
-
+import clsx from 'clsx';
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 const Header = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const user = useSelector(selectUser);
@@ -13,12 +16,20 @@ const Header = () => {
       <h3>Redux Toolkit</h3>
       {isLoggedIn && <div>{user.email}</div>}
       <ul>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/tasks'>Tasks</NavLink>
+        <NavLink className={buildLinkClass} to='/'>
+          Home
+        </NavLink>
+        <NavLink className={buildLinkClass} to='/tasks'>
+          Tasks
+        </NavLink>
         {!isLoggedIn && (
           <>
-            <NavLink to='/login'>Login</NavLink>
-            <NavLink to='/register'>Register</NavLink>
+            <NavLink className={buildLinkClass} to='/login'>
+              Login
+            </NavLink>
+            <NavLink className={buildLinkClass} to='/register'>
+              Register
+            </NavLink>
           </>
         )}
         {isLoggedIn && <button onClick={() => dispatch(logout())}>Logout</button>}
